0051b8797ceff7c9af5a8accb298f0be01658fcb
Juan Rodriguez <jrodriguezvalencia@popular.com>
2022-08-13 14:07:24 -0400

    Intial setup up to "GET" "POST" Requests

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,10 @@
-from flask import Blueprint , render_template
+from crypt import methods
+from operator import methodcaller
+from flask import Blueprint , render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
   return render_template("login.html")
 
@@ -10,6 +12,24 @@ def login():
 def logout():
   return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+  if request.method == 'POST':
+    email  = request.form.get('email')
+    firstName = request.form.get('fistName')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    
+    if len(email) < 4:
+      pass
+    elif len(firstName) < 2:
+      pass
+    elif password1 != password2:
+      pass
+    else:
+      # add user to database
+      pass
+    
+    
+    
   return render_template("sign_up.html")
